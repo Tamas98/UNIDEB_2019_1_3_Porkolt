f153842d07cf9b5549255d9dd7c37221b4c060de
Bunyi5 <bbakos262@gmail.com>
2019-09-28 11:24:00 +0200

    pom.xml jÃÃavítása

---
+++ Project Files/target/clover/src/Modell/Language.java
@@ -0,0 +1,10 @@
+/* $$ This file has been instrumented by Clover 4.3.1#20180921211537623 $$ */package Modell;
+
+import java.util.Map;
+
+public class Language {
+
+    String name;
+
+    Map<String,String> words;
+}
---
+++ Project Files/target/clover/src/Modell/User.java
@@ -0,0 +1,14 @@
+/* $$ This file has been instrumented by Clover 4.3.1#20180921211537623 $$ */package Modell;
+
+import java.util.Map;
+
+public class User {
+
+    String username;
+
+    String password;
+
+    String restore;
+
+    Map<String,String> learned;
+}
