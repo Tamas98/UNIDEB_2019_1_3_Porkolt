63886c1e3145e57bdde8e59f6a7ce19209b03520
Kokusz66 <99letter86@gmail.com>
2019-09-29 13:52:23 +0200

    pom-ban version módisítás, exec.mainClass módosítás, MainApplication létrehozása

--- Project Files/src/main/java/Main.java
+++ Project Files/src/main/java/Main.java
@@ -1,21 +1,8 @@
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
 
-public class Main extends Application {
-
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("fxml/Udvozlo.fxml"));
-        primaryStage.setTitle("Nyelvtanulás");
-        primaryStage.setScene(new Scene(root, 600, 400));
-        primaryStage.show();
-    }
-
+public class Main{
 
     public static void main(String[] args) {
-        launch(args);
+        Application.launch(MainApplication.class, args);
     }
 }
